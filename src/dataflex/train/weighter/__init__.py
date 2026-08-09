6c338b4afd2dbdd7ec2ba0d50b91f6ccabe08433
tongruiliu <952814125@qq.com>
2026-08-09 23:09:55 +0800

    Add joint_update_aware weighter (#58)
    * add joint_update_aware weighter
    
    * add joint_update_aware weighter

--- src/dataflex/train/weighter/__init__.py
+++ src/dataflex/train/weighter/__init__.py
@@ -2,3 +2,4 @@ from .base_weighter import Weighter
 from .loss_weighter import *
 from .custom_weighter import CustomWeighter
 from .adapt_weighter import AdaptWeighter
+from .joint_update_aware_weighter import JointUpdateAwareWeighter
